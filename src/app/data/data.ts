e6167aaa652e111cd2acaf7148c6f8f5435a1814
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2023-10-20 16:32:28 -0300

    Usando o ngFor para exibir o App-card e diminuir o codigo

---
+++ src/app/data/data.ts
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        "gameCover": "assets/bt-1.jpg",
+        "gameLabel": "DIGITAL",
+        "gameType": "DIGITAL | PS4",
+        "gamePrice": "R$ 299,00"
+    },
+    {
+        "gameCover": "assets/bt-4.jpg",
+        "gameLabel": "EXCLUSIVE",
+        "gameType": "DISC | PS4",
+        "gamePrice": "R$ 199,00"
+    },
+    {
+        "gameCover": "assets/bt-5.jpg",
+        "gameLabel": "BEST OF YEAR",
+        "gameType": "DISC | PS4",
+        "gamePrice": "R$ 599,00"
+    },
+    {
+        "gameCover": "assets/bt-bad-company-2.jpg",
+        "gameLabel": "NEW",
+        "gameType": "DISC | PS4 | PS5",
+        "gamePrice": "R$ 199,99"
+    },
+    {
+        "gameCover": "assets/bt-hardline.jpg",
+        "gameLabel": "EXCLUSIVE",
+        "gameType": "DISC | PS4 | PS5",
+        "gamePrice": "R$ 250,00"
+    },
+    {
+        "gameCover": "assets/ac-cover.jpg",
+        "gameLabel": "NEW",
+        "gameType": "DIGITA | PS4 | PS5",
+        "gamePrice": "R$ 90,00"
+    }
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { data } from 'src/app/data/data';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+ gameData = data 
   constructor() { }
 
   ngOnInit(): void {
+  
   }
 
 }
